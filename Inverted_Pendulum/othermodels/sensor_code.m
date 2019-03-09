bdf52c904fcef6fab3483e74a1db4201dd9d97a0
Jiyo Palatti <jlfamily92@gmail.com>
2019-03-09 13:19:02 +0200

    REaaranges files

--- Inverted_Pendulum/othermodels/sensor_code.m
+++ Inverted_Pendulum/othermodels/sensor_code.m
@@ -5,8 +5,11 @@ persistent y
 switch seg
  case 1
   y = ttAnalogIn(1);
-  ttAnalogOut(1, y);
   exectime = 0.0005;
  case 2
+  disp(["Sending MEASUREMENT" y])
+  ttSendMsg(3, y, 80); % Send message (80 bits) to node 3 (controller)
+  exectime = 0.0004;
+ case 3
   exectime = -1; % finished
 end
